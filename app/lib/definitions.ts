8fac1cada29a81f7bdfe14ac883cb6fc0c86052f
Channaveer <channaveerbs0905@gmail.com>
2024-09-28 20:26:17 +0530

    first commit

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -43,7 +43,7 @@ export type LatestInvoice = {
 export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
   amount: number;
 };
-
+ 
 export type InvoicesTable = {
   id: string;
   customer_id: string;
